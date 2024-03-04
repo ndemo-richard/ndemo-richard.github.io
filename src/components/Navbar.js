9c5ba2b7d42e08171aca62dfabc1ed135af00913
ndemo-richard <ndemorich@gmail.com>
2024-03-04 19:43:28 +0300

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,7 +24,11 @@ const Navbar = () => {
           </div>
           <div className="md:hidden flex items-center">
             <button className="outline-none mobile-menu-button" aria-label="Open Menu" onClick={() => setIsOpen(!isOpen)}>
-              {/* SVG icon */}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-menu">
+              <line x1="3" y1="12" x2="21" y2="12"></line>
+              <line x1="3" y1="6" x2="21" y2="6"></line>
+              <line x1="3" y1="18" x2="21" y2="18"></line>
+            </svg>
             </button>
           </div>
         </div>
